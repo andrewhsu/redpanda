1c9811b5c44bd62f7d8124b6eea0f3b776aed362
Andrew Hsu <xuzuan@gmail.com>
2022-08-27 01:49:39 +0000

    asdf

---
+++ .github/workflows/scripts/smoke-check-rel/check-assets.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x -e -o pipefail
+if [[ -z "$1" ]]; then
+	echo error: required argument redpanda version, e.g. 22.2.1
+	exit 1
+fi
+RELEASE_ASSETS=$(gh release view --json 'assets' --jq '.assets | length' "v$1")
+(( $RELEASE_ASSETS == 5 ))
