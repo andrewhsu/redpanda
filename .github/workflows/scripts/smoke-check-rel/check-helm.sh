496b1ef19801208045c7b59ae8f58ecf3e1ae145
Andrew Hsu <xuzuan@gmail.com>
2022-08-27 00:58:03 +0000

    asdf

---
+++ .github/workflows/scripts/smoke-check-rel/check-helm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x -e -o pipefail
+if [[ -z "$1" ]]; then
+	echo error: required argument redpanda version, e.g. 22.2.1
+	exit 1
+fi
+helm repo add redpanda https://charts.vectorized.io/
+helm repo update
+HELM_SEARCH_APP_VERSION=$(helm search repo redpanda --version "v$1" --output json | jq -r '.[].app_version')
+[[ $HELM_SEARCH_APP_VERSION =~ ^v$1 ]]
